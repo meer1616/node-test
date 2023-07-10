dc7b79e7e23150501a04ee6b80340638bb0919d8
SPT-Meer <meer.spt@gmail.com>
2023-07-10 18:25:55 +0530

    intiial test commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+
+app.get("/",async(req,res)=>{
+    res.send("Hi there this is node app !!")
+})
+
+app.get("/api",async(req,res)=>{
+    res.send("Hi there this is api route!!")
+})
+
+
+app.listen(8000,()=>{
+    console.log(`server is listening http://localhost:8000`);
+})
